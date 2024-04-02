224f3fadaa9fb9d090fb525340999d46f9f6df67
Akshatachavan123 <107571120+Akshatachavan123@users.noreply.github.com>
2024-04-02 13:35:03 +0530

    Add files via upload

---
+++ numberpattern1.java
@@ -0,0 +1,26 @@
+package loopstatement;
+
+public class numberpattern1 {
+
+	public static void main(String[] args) {
+		int count=0;
+		
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=i;j>=1;j--){
+				
+				count+=1;
+				System.out.print(count+" ");
+			
+			}
+			
+			System.out.println();
+		}
+		
+		
+		
+		
+
+	}
+
+}
